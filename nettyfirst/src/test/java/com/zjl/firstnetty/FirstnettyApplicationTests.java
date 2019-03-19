71e87a3ce5098d42bbcb590e3e4a92c8fc7c7544
13588330822 <13588330822@163.com>
2019-03-19 12:52:31 +0800

    初次提交

---
+++ nettyfirst/src/test/java/com/zjl/firstnetty/FirstnettyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zjl.firstnetty;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FirstnettyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
